e629629a3ae6ecdcc7e45af07a8a6fcabc1a9f66
Dipankar Pal <dipankarpal5050@gmail.com>
2020-05-17 15:09:11 +0000

    stories added

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ if (!fs.existsSync('./metadata')) {
 
 
 var todays_url = ""
-var archive_url = "https://www.anandabazar.com/supplementary/rabibashoriyo/archive?page=20&slab=0&tnp=50";
+var archive_url = "https://www.anandabazar.com/supplementary/rabibashoriyo/archive?page=21&slab=0&tnp=50";
 
 
 
