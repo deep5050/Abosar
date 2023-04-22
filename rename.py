37bc8574e6cdb18512008f2080148af6d527498d
Dipankar Pal <dipankarpal5050@gmail.com>
2023-04-22 18:30:23 +0000

    more keywords added

--- rename.py
+++ rename.py
@@ -1,12 +1,12 @@
 import os
 
-directory = './stories/rabibasariya/'
+directory = './metadata/rabibasariya/'
 
 for filename in os.listdir(directory):
     print(filename)
 
-    new_filename = filename.replace('Short-story:-', '')
-    new_filename = filename.replace('short-story:-', '')
-    new_filename = filename.replace('Short-Story:-', '') 
-    os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
+    new_filename = filename.replace('A-short-story:-', '')
+    new_filename = new_filename.replace('short-story:-', '')
+    new_filename = new_filename.replace('Short-story:-', '') 
+    os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename.strip()))
     print("renaming...")
